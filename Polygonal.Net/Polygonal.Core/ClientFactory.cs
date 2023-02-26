6368e6cff04d9754940bdb11afe550bd7338317b
cimib <cimib@outlook.com>
2023-02-26 14:57:21 -0800

    Initial changes.

---
+++ Polygonal.Net/Polygonal.Core/ClientFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polygonal.Core
+{
+    public static class ClientFactory
+    {
+        public static PolygonClient GetClient()
+        {
+            throw NotImplementedException();
+        }
+
+        public static void RegisterCustomHttpClient(HttpClient httpClient)
+        {
+
+        }
+    }
+}
---
+++ Polygonal.Net/Polygonal.Core/PolygonClient.cs
@@ -0,0 +1,22 @@
+﻿namespace Polygonal.Core
+{
+    public class PolygonClient : IDisposable
+    {
+        private bool disposeHttpClient;
+        private HttpClient httpClient;
+
+        public PolygonClient(HttpClient httpClient, bool disposeHttpClient)
+        {
+            this.httpClient = httpClient;
+            this.disposeHttpClient = disposeHttpClient;
+        }
+
+        public void Dispose()
+        {
+            if (this.disposeHttpClient)
+            {
+                this.httpClient.Dispose();
+            }
+        }
+    }
+}
